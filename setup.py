fc523a94237e79cf366b2a98e7468ff77ccdf541
Vaibhav Karve <vaibhavskarve@gmail.com>
2019-08-21 14:01:57 -0500

    (split)Fix broken setup.py file

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 
+import os
 from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name='graphsat',
       version='0.1',

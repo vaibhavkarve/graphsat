bc0aaf99e5727b78614d7b8c488d2bc54ab055bc
Vaibhav Karve <vaibhavskarve@gmail.com>
2020-08-05 12:28:05 -0500

    (split)add gor() and gand() for conjunction and disjunction of CNF-sets.

--- operations.py
+++ operations.py
@@ -49,6 +49,33 @@ def sat_or(graph1: Union[bool, MHGraph, AtomicSxpr],
     return satg(graph1) or satg(graph2)
 
 
+def gor(graph1: Union[MHGraph, Set[cnf.CNF]],
+        graph2: Union[MHGraph, Set[cnf.CNF]]) -> Set[cnf.CNF]:
+    """Disjunct the corresponding CNFs."""
+    if not isinstance(graph1, set):
+        graph1 = set(sat.cnfs_from_mhgraph(mhgraph(graph1)))
+    if not isinstance(graph2, set):
+        graph2 = set(sat.cnfs_from_mhgraph(mhgraph(graph2)))
+
+    product = it.product(graph1, graph2)
+    disjunction = it.starmap(prop.cnf_or_cnf, product)
+    disjunction_reduced = map(cnf.tautologically_reduce_cnf, disjunction)
+    return set(disjunction_reduced)
+
+
+def gand(graph1: Union[MHGraph, Set[cnf.CNF]],
+        graph2: Union[MHGraph, Set[cnf.CNF]]) -> Set[cnf.CNF]:
+    """Conjunct the corresponding CNFs."""
+    if not isinstance(graph1, set):
+        graph1 = set(sat.cnfs_from_mhgraph(mhgraph(graph1)))
+    if not isinstance(graph2, set):
+        graph2 = set(sat.cnfs_from_mhgraph(mhgraph(graph2)))
+
+    product = it.product(graph1, graph2)
+    conjunction = it.starmap(prop.cnf_and_cnf, product)
+    conjunction_reduced = map(cnf.tautologically_reduce_cnf, conjunction)
+    return set(conjunction_reduced)
+
 if __name__ == "__main__":
     from time import time
     with logger.catch(message="Something unexpected happened ..."):

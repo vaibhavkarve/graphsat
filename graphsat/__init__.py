edacd0a7491c62d955cd52c84baba5ffbd09d5dc
Vaibhav Karve <vaibhavskarve@gmail.com>
2021-08-24 07:31:18 -0400

    remove sys.path modification made in __init__

--- graphsat/__init__.py
+++ graphsat/__init__.py
@@ -1,7 +1,1 @@
 #! /usr/bin/env python3.9
-"""For fixing import errors."""
-import sys
-
-# local imports
-sys.path.insert(0, './')  # Add graphsat package to path
-sys.path.insert(0, '~/projects/graphsat/graphsat')  # Add graphsat package to path

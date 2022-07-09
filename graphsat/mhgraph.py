5e46fafced64d080a22042ed15021d2004ab6ded
Vaibhav Karve <vkarve@protonmail.com>
2022-07-09 15:19:06 -0400

    code-cleanup

--- graphsat/mhgraph.py
+++ graphsat/mhgraph.py
@@ -26,55 +26,16 @@ An *HGraph* is a MHGraph without HEdge-multiplicities.
 # Imports from standard library.
 from collections import Counter as counter
 from typing import (AbstractSet, Collection, Counter, FrozenSet, List, NewType,
-                    Optional, Tuple, TypeVar, Union)
+                    Optional, Tuple, TypeVar, Sequence, Union, Mapping)
+from typing_extensions import reveal_type
 
 # Imports from third-party modules.
-import anytree as at  # type: ignore
+import anytree as at
+from attr import define
 from loguru import logger
 
 # Imports from local modules.
 from graphsat.graph import Graph, GraphType, Vertex, graph, vertex
-
-#: ``T = TypeVar('T')``, i.e. ``T`` is a type variable.
-T = TypeVar('T')  # pylint: disable=invalid-name
-
-# MHGraphType (Hashable Counter) for Storing MHGraphs
-# ===================================================
-# (for internal use only, partially documented)
-class MHGraphType(Counter[AbstractSet[T]]):
-    """`MHGraphType[_T]` is a subclass of `collections.Counter[AbstractSet[_T]]`.
-
-    It overrides Counter's ``__hash__`` and ``__repr__`` method, making it hashable.
-
-    Implementation inspired by Raymond Hettinger's
-    `solution <https://stackoverflow.com/questions/1151658/python-hashable-dicts>`_.
-    Implementing ``__hash__`` enables us to put it inside containers like `Dict` or `Set`.
-
-    """
-
-    def __hash__(self) -> int:  # type: ignore
-        """Hash function that depends only on the keys of the Counter, ignoring values."""
-        return hash(frozenset(self))
-
-    def __repr__(self) -> str:
-        """Print the MHGraphType in a compact way."""
-        unicode_superscripts: dict[int, str]
-        unicode_superscripts = {1: '\u00b9', 2: '\u00b2', 3: '\u00b3', 4: '\u2074',
-                                5: '\u2075', 6: '\u2076', 7: '\u2077', 8: '\u2078',
-                                9: '\u2079'}
-
-        ordered_hedges: list[AbstractSet[T]]
-        ordered_hedges = sorted(sorted(super().keys()), key=len)  # pylint: disable=no-member
-
-        def superscript(hedge_: AbstractSet[T]) -> str:
-            multiplicity: int = super(MHGraphType, self).get(frozenset(hedge_), 0)
-            return unicode_superscripts.get(multiplicity, f'^{multiplicity}')
-
-        hedge_strings: list[str]
-        hedge_strings = [str(h) + superscript(h) for h in ordered_hedges]
-
-        return ','.join(hedge_strings)
-
 
 
 # Classes and Types
@@ -90,11 +51,32 @@ class HEdge(FrozenSet[Vertex]):  # pylint: disable=too-few-public-methods
 HGraph = NewType('HGraph', GraphType[Vertex])
 HGraph.__doc__ = """`HGraph` is a subtype of `PreGraph[Vertex]`."""
 
-class MHGraph(MHGraphType[Vertex]):  # pylint: disable=too-few-public-methods
-    """`MHGraph` is a subclass of `MHGraphType[Vertex]`."""
+
+class MHGraph(Counter[HEdge]):
+    def __hash__(self) -> int:  # type: ignore
+        """Hash function that depends only on the keys of the Counter, ignoring values."""
+        return hash(frozenset(self))
+
+    def __repr__(self) -> str:
+        """Print the MHGraphType in a compact way."""
+        unicode_superscripts: dict[int, str]
+        unicode_superscripts = {1: '\u00b9', 2: '\u00b2', 3: '\u00b3', 4: '\u2074',
+                                5: '\u2075', 6: '\u2076', 7: '\u2077', 8: '\u2078',
+                                9: '\u2079'}
+
+        ordered_hedges: list[HEdge] = sorted(sorted(super().keys()), key=len)  # pylint: disable=no-member
+
+        def superscript(hedge_: HEdge) -> str:
+            multiplicity: int = self.get(hedge_, 0)
+            return unicode_superscripts.get(multiplicity, f'^{multiplicity}')
+
+        hedge_strings: list[str]
+        hedge_strings = [str(h) + superscript(h) for h in ordered_hedges]
+
+        return ','.join(hedge_strings)
 
 
-class GraphNode(at.NodeMixin):  # type: ignore
+class GraphNode(at.NodeMixin):
     """This is a MHGraph that can also act as the node in a tree.
 
     API for interpreting MHGraphs rewriting as trees.
@@ -122,7 +104,7 @@ class GraphNode(at.NodeMixin):  # type: ignore
 
     def __str__(self) -> str:
         """Pretty print a tree."""
-        lines = []
+        lines: list[str] = []
         for pre, _, node in at.RenderTree(self):
             #pre = ' ' + pre if pre else pre  # add some padding to the front
             line = (f'{pre} {node.graph} '
@@ -138,7 +120,7 @@ class GraphNode(at.NodeMixin):  # type: ignore
 # =====================
 
 
-def hedge(vertex_collection: Collection[int]) -> HEdge:
+def hedge(vertex_collection: Collection[int | Vertex] | HEdge) -> HEdge:
     """Constructor-function for HEdge type.
 
     For definition of HEdge, refer to :ref:`definitionofagraph`.
@@ -161,7 +143,7 @@ def hedge(vertex_collection: Collection[int]) -> HEdge:
     return HEdge(frozenset(map(vertex, vertex_collection)))
 
 
-def hgraph(hedge_collection: Collection[Collection[int]]) -> HGraph:
+def hgraph(hedge_collection: Collection[Collection[int]] | HGraph) -> HGraph:
     """Constructor-function for HGraph type.
 
     A HGraph, is a MHGraph without HEdge-multiplicities.
@@ -184,7 +166,7 @@ def hgraph(hedge_collection: Collection[Collection[int]]) -> HGraph:
     return HGraph(GraphType(set(map(hedge, hedge_collection))))
 
 
-def mhgraph(edge_collection: Collection[Collection[int]]) -> MHGraph:
+def mhgraph(edge_collection: Collection[Collection[int | Vertex]]) -> MHGraph:
     """Constructor-function for MHGraph type.
 
     For definition of a MHGraph, refer to :ref:`definitionofagraph`.
@@ -209,17 +191,17 @@ def mhgraph(edge_collection: Collection[Collection[int]]) -> MHGraph:
 
     try:
         # edge_collection is a Counter.
-        return MHGraph(MHGraphType(map(hedge, edge_collection.elements())))  # type: ignore
+        return MHGraph(map(hedge, edge_collection.elements()))  # type: ignore
     except AttributeError:
         # edge_collection is not a Counter.
-        return MHGraph(MHGraphType(map(hedge, edge_collection)))
+        return MHGraph(map(hedge, edge_collection))
 
 
 # Basic Functions
 # ===============
 
 
-def vertices(mhg: Union[HGraph, MHGraph]) -> FrozenSet[Vertex]:
+def vertices(mhg: HGraph | MHGraph) -> FrozenSet[Vertex]:
     """Return a `frozenset` of all vertices of a MHGraph."""
     return frozenset.union(*mhg)
 
@@ -269,7 +251,7 @@ def sphr(mhg: MHGraph, vertex: Vertex) -> Tuple[HEdge, ...]:
     return tuple(hedge(h) for h in mhg.elements() if vertex not in h)
 
 
-def graph_union(mhg1: Tuple[HEdge, ...], mhg2: Tuple[HEdge, ...]) -> MHGraph:
+def graph_union(mhg1: Sequence[HEdge] | MHGraph, mhg2: Collection[HEdge] | MHGraph) -> MHGraph:
     """Union of the two graphs."""
     assert mhg1 or mhg2, f'Encountered empty input {mhg1 = } or {mhg2 = }'
     return mhgraph(mhg1 + mhg2)
@@ -333,7 +315,7 @@ def mhgraph_from_graph(graph_instance: Graph) -> MHGraph:
     return mhgraph(graph_instance)
 
 
-if __name__ == '__main__':
+if __name__ == '__main__':  # pragma: no cover
     logger.info('MHGraphs can be constructed using the mhgraph() function.')
     logger.info('>>> mhgraph([[1, 2, 3], [1, 2], [2, 3], [3, 1], [3, 2]])')
     logger.info(mhgraph([[1, 2, 3], [1, 2], [2, 3], [3, 1], [3, 2]]))

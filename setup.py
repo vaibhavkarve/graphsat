524554489016f0469b3b6abaa069cdfc91f4b887
Vaibhav Karve <vkarve@protonmail.com>
2022-09-20 20:56:31 -0400

    remove setup.py

--- setup.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3.10
-"""This setup file is needed for installing the `graphsat` package in editable mode."""
-
-from setuptools import setup
-
-if __name__ == '__main__':
-    setup()
